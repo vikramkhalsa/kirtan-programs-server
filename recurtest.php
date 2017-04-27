86494d4168240f9b8fa7e4c945324d36ce08060d
VSK <vikramkhalsa@outlook.com>
2017-04-26 18:49:27 -0700

    WIp recurrence implementation

--- recurtest.php
+++ recurtest.php
@@ -18,63 +18,81 @@ $sql = $sql." ORDER BY sd ASC";
 
     while($row=mysqli_fetch_assoc($result))
     {
-        $array[] = $row;
-        echo $row["rrule"];
-        echo '<br>';
+
         if ($row['rrule']!=null){
-          echo $row['title'];
-             echo '<br>';
-          $event = $row;
-        }
+          //echo $row['title'];
+          array_push($array,getRecurEvents($row));
+        }else {
+
+         $array[] = $row;
+       }
     }
 
+echo json_encode($array);
+function getRecurEvents($event){
 
+$events = array();
 
-
-
-$rulestr = $event['rrule'];//"FREQ=DAILY";//;COUNT=5;INTERVAL=3";
-
+//$rulestr = $event['rrule'];//"FREQ=DAILY";//;COUNT=5;INTERVAL=3";
+$rulestr = "FREQ=DAILY;UNTIL=2017-05-10";//;COUNT=5;INTERVAL=3";
 $startdate = new DateTime($event['sd']);
 $enddate = new DateTime($event['ed']);
-
+$duration = "2:30";
+$duration = explode(':',$duration);
+$durHrs = $duration[0];
+$durMins = $duration[1];
   // Recurring event, parse RRULE and add appropriate duplicate events
-                $rrules = array();
-                $rruleStrings = explode(';', $rulestr);
-                foreach ($rruleStrings as $s) {
-                    list($k, $v) = explode('=', $s);
-                    $rrules[$k] = $v;
-                }
+$rrules = array();
+$rruleStrings = explode(';', $rulestr);
+foreach ($rruleStrings as $s) {
+         list($k, $v) = explode('=', $s);
+           $rrules[$k] = $v;
+         }
 
-     // Get frequency
-                $frequency = $rrules['FREQ'];            
-
-  $startTimestamp = "";
-    $endTimestamp="";
-     $duration="";
+// Get frequency
+$frequency = $rrules['FREQ'];            
 
      // Get Interval
-                $interval = (isset($rrules['INTERVAL']) && $rrules['INTERVAL'] !== '')
+      $interval = (isset($rrules['INTERVAL']) && $rrules['INTERVAL'] !== '')
                     ? $rrules['INTERVAL']
                     : 1;
-                    
-                    //get Count
-                   $count = (isset($rrules['COUNT']) && $rrules['COUNT'] !== '')
+ //get Count
+   $count = (isset($rrules['COUNT']) && $rrules['COUNT'] !== '')
                     ? $rrules['COUNT']
                     : 1;
+
+$until=$enddate;
+                if (isset($rrules['UNTIL'])) {
+                    // Get Until
+                    $until = new DateTime($rrules['UNTIL']);    
+                    }           
  // Decide how often to add events and do so
                 switch ($frequency) {
                     case 'DAILY':
-                      $newsd = $startdate->add(new DateInterval('P'.$interval.'D'));
-                    while ($newsd<$enddate){
-                    	//for($i=0;$i<$count;$i++){
-                    	$tempevent = $event;
-                      $tempevent['sd'] = $newsd->format('Y-m-d H:i');
-                      echo json_encode($tempevent);
-                    		//echo $newsd->format('Y-m-d H:i');
-                    		echo '<br>';
+                      $newsd = $startdate;//->add(new DateInterval('P'.$interval.'D'));
+                      $tempd = clone $newsd;
+                       $newed = $tempd->add(new DateInterval('PT'.$durHrs.'H'.$durMins.'M'));
+                    while ($newsd<=$until){
+                      //for($i=0;$i<$count;$i++){
+                      $tempevent = $event;
+                      $tempevent['sd'] = $newsd->format('Y-m-d H:i'); 
+                      $tempevent['ed'] = $newed->format('Y-m-d H:i');
+                      echo $tempevent['sd'].' '.$tempevent['ed'];
+                      echo '<br>';
+                      $events[] = $tempevent;
+
+                      //echo json_encode($tempevent);
+                        //echo $newsd->format('Y-m-d H:i');
+                        //echo '<br>';
                         $newsd = $startdate->add(new DateInterval('P'.$interval.'D'));
-                    	} 
+                              $tempd = clone $newsd;
+                         $newed = $tempd->add(new DateInterval('PT'.$durHrs.'H'.$durMins.'M'));
+                      } 
                     break;
                }
+               return $events;
+}
+
+
 
 ?>
